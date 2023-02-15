8daf91b085241d7cbe6af9e91fed7f2d67937950
Nibras Misk <nibrassmisk@gmail.com>
2023-02-15 11:27:30 +0200

    ex-4.1

--- all-exercises/src/App.js
+++ all-exercises/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Box1 from "./componants/Box1";
+import Box2 from "./componants/Box2";
+import Box3 from "./componants/Box3";
+import Box4 from "./componants/Box4";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Box1></Box1>;
 }
 
 export default App;
---
+++ all-exercises/src/componants/Box4.js
@@ -0,0 +1,7 @@
+import "./Box4.css";
+
+function Box4() {
+  return <div className="box4">Box4</div>;
+}
+
+export default Box4;
