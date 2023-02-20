84333436c89e2d7eb64b53a0339e18d3c4e8219e
Nibras Misk <nibrassmisk@gmail.com>
2023-02-20 15:23:46 +0200

    ex-13.3

--- all-exercises/src/components/ChangingBox.jsx
+++ all-exercises/src/components/ChangingBox.jsx
@@ -4,12 +4,32 @@ import "./ChangingBox.css";
 
 export default function ChangingBox() {
   const [cls1, setCls1] = useState("");
+  const [counter, setCounter] = useState(0);
+
+  const colors = ["red", "green", "blue", "yellow", "plum"];
+
+  const change = (color) => {
+    setCls1(color);
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      change(colors[counter]);
+      setCounter(counter + 1);
+    }, 1000);
+  }, []);
 
   useEffect(() => {
     setTimeout(() => {
-      setCls1("box1");
+      if (counter < colors.length) {
+        change(colors[counter]);
+        setCounter(counter + 1);
+      }
+      if (counter == colors.length) {
+        change("circle");
+      }
     }, 1000);
-  });
+  }, [cls1]);
+
   return (
     <div>
       <div className={cls1}></div>
