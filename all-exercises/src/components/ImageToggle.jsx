01f39c428199b68e551d7c4ccaccc59bc1ea5ef1
Nibras Misk <nibrassmisk@gmail.com>
2023-02-20 16:48:28 +0200

    ex-10.2

--- all-exercises/src/components/ImageToggle.jsx
+++ all-exercises/src/components/ImageToggle.jsx
@@ -1,12 +1,18 @@
 import coloredImg from "../images/img1.jpg";
 import balckImg from "../images/img1-black.jpg";
-
+import "./ImageToggle.css";
+import { useRef } from "react";
 export default function ImageToggle() {
+  const imgRef = useRef(null);
   return (
     <div>
       <h1>My Img</h1>
-      <img src={coloredImg} width="50%" height="50%" />
-      <img src={balckImg} width="50%" height="50%" />
+      <img
+        ref={imgRef}
+        src={balckImg}
+        onMouseEnter={() => (imgRef.current.src = coloredImg)}
+        onMouseLeave={() => (imgRef.current.src = balckImg)}
+      />
     </div>
   );
 }
