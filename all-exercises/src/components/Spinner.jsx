076f8e6d3e1a51e7c026dd3631d5df7d21f55002
Nibras Misk <nibrassmisk@gmail.com>
2023-02-20 16:09:21 +0200

    ex-13.4

--- all-exercises/src/components/Spinner.jsx
+++ all-exercises/src/components/Spinner.jsx
@@ -1,8 +1,23 @@
 import "./Spinner.css";
+import { useRef, useEffect } from "react";
+import { useState } from "react";
 export default function Spinner() {
+  const loading = useRef(null);
+
+  const spinners = ["loader1", "loader2", "loader3"];
+
+  let rand = Math.floor(Math.random() * 3);
+
+  const [cls, setCls] = useState(spinners[rand]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      loading.current.classList.remove(cls);
+    }, 5000);
+  }, []);
   return (
     <div>
-      <div className="loader"></div>
+      <div ref={loading} className={cls}></div>
     </div>
   );
 }
