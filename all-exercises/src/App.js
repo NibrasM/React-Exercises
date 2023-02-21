70d6f1114ab054435b2f53f29c7da77e785869b2
Nibras Misk <nibrassmisk@gmail.com>
2023-02-21 10:16:44 +0200

    routes-code-with-yishai

--- all-exercises/src/App.js
+++ all-exercises/src/App.js
@@ -1,8 +1,43 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import Admin from "./components/Admin";
+import Child from "./components/Child";
+import Parent from "./components/Parent";
 
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/admin",
+      element: <Admin />,
+    },
+    {
+      path: "*",
+      element: <>Not Found</>,
+    },
+    {
+      path: "/parent",
+      children: [
+        {
+          path: "",
+          element: <Parent></Parent>,
+        },
+        {
+          path: "child",
+          element: <Child></Child>,
+        },
+      ],
+    },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
