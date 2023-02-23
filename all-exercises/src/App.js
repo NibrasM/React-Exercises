ee5cf60147ab3c064d8d107e56b57b1702d6c240
Nibras Misk <nibrassmisk@gmail.com>
2023-02-23 11:08:37 +0200

    ex-17.1

--- all-exercises/src/App.js
+++ all-exercises/src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ToDoList from "./Components/ToDoList";
+import Todo2 from "./Components/Todo2";
 
 function App() {
   return (
     <div>
-      <ToDoList></ToDoList>
+      <Todo2></Todo2>
     </div>
   );
 }
