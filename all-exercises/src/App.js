afc611118071bdfd882168fc0d131289ae60be69
Nibras Misk <nibrassmisk@gmail.com>
2023-02-27 14:12:31 +0200

    ex-21

--- all-exercises/src/App.js
+++ all-exercises/src/App.js
@@ -1,8 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import BoxAnimation from "./components/BoxAnimation";
+import Text from "./components/Text";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BoxAnimation size={170} />
+
+      {/* <Text /> */}
+    </div>
+  );
 }
 
 export default App;
